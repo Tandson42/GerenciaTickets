0fa6793fe912a1df15ec274fa7b4c844638fbd1c
tandson <tandsonfilho@gmail.com>
2026-02-11 22:13:25 -0300

    adicionando tratamento customizado de erros JSON na API

--- chamados/bootstrap/app.php
+++ chamados/bootstrap/app.php
@@ -3,6 +3,11 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Auth\Access\AuthorizationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +20,37 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                $previous = $e->getPrevious();
+
+                if ($previous instanceof ModelNotFoundException) {
+                    $model = class_basename($previous->getModel());
+
+                    return response()->json([
+                        'message' => "Recurso {$model} não encontrado.",
+                    ], 404);
+                }
+
+                return response()->json([
+                    'message' => 'Recurso não encontrado.',
+                ], 404);
+            }
+        });
+
+        $exceptions->render(function (AuthorizationException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => 'Você não tem permissão para realizar esta ação.',
+                ], 403);
+            }
+        });
+
+        $exceptions->render(function (AuthenticationException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => 'Não autenticado. Faça login para continuar.',
+                ], 401);
+            }
+        });
     })->create();
